47c67c0a1981472374fb9d131d8d96ae0e73c6ee
Lukáš Kaplánek <146857173+spl1x1@users.noreply.github.com>
2025-10-17 09:27:47 +0200

    macsync

---
+++ server/Server.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Lukáš Kaplánek on 17.10.2025.
+//
+
+#include "Server.h"
+
+class Server {
+
+    int port;
+    Socket serverSocket;
+    bool isRunning;
+
+public:
+Server() {
+    if (isRunning) {
+        return;
+    }
+}
+
+#ifdef __fromPython__:
+
+#else
+
+#endif
+
+};
---
+++ server/Server.h
@@ -0,0 +1,23 @@
+//
+// Created by Lukáš Kaplánek on 17.10.2025.
+//
+
+#ifndef SERVER_H
+#define SERVER_H
+
+#ifdef __fromPython__:
+#include <Python.h>
+#endif
+
+
+
+class Server {
+
+
+
+
+};
+
+
+
+#endif //SERVER_H
