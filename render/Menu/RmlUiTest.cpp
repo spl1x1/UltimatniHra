33d3584deea0f78d5ed481aa1e5818b2d3c6fb12
Lukáš Kaplánek <146857173+spl1x1@users.noreply.github.com>
2025-10-20 13:59:42 +0200

    Add Main Menu template and style file

--- render/Menu/RmlUiTest.cpp
+++ render/Menu/RmlUiTest.cpp
@@ -5,6 +5,33 @@
 #include <FileSystem>
 #include <iostream>
 
+static const char* document_rml = R"(
+<rml>
+<head>
+<style>
+    body {
+        font-family: rmlui-debugger-font;
+        font-size: 12px;
+        top: 100px; right: 100px; bottom: 100px; left: 100px;
+        padding: 150px;
+        text-align: center;
+        background: #333;
+        color: white;
+    }
+    p {
+        display: block;
+        padding: 30px;
+    }
+    p:hover {
+        background: #444;
+    }
+</style>
+</head>
+<body>
+    <p>Hello world!</p>
+</body>
+</rml>
+)";
 
 int main() {
 	Backend::Initialize("Znake", 1024, 768, true);
